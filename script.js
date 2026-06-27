a21bd8352c646c37701c7b9ccb0ff5e98eaa04c7
matsimo <maxlert2011xd@gmail.com>
2026-06-27 17:58:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-        window.addEventListener("load",changeBackground);
+window.addEventListener("load",changeBackground);
+
         function changeBackground(){
+                
             const listImages=["cataratas.jpg","machu-picchu.jpg","perito_moreno.jpg","roquez.jpg","tatacoa.jpg","titicaca.jpg"];
             const index=Math.floor(Math.random() * listImages.length);
             const selectedImage=listImages[index];
             const imagePath="main/"+selectedImage;
+                
             document.body.style.backgroundImage= `url('${imagePath}')`;
+                
             //mostrar descripcion de la imagen
             const description= getDescription(index);
             document.getElementById("description").textContent=description;
+                
             function getDescription(idx){
+                    
                 switch (idx){
+                                
                     case 0:
                         return"Cataratas del iguazú,argentina";
                     case 1:
